9e74e02d1f9b7c49833f8b4125669db0885b0e33
Artem Novichkov <novichkoff93@gmail.com>
2019-09-11 22:33:54 +0600

    Add UIKit check

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "texstyle",
+    platforms: [
+    	.iOS(.v9), .tvOS(.v9)
+    ],
     products: [
         .library(name: "texstyle", targets: ["texstyle"])
     ],
